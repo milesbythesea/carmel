7dffb45d84878937c9d71e12a3b96b47d5c6c418
milesbythesea <keithmilesbr@icloud.com>
2025-07-20 18:41:33 +0000

    add stlying/ images

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,20 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h2>
-milesByTheSea       
+miles by the sea       
  </h2>
         <p>
 <p>comming soon....</p>
         </p>
       </header>
+      <div className="App-body">
+        </div>
+        <div className="img-container">
+        <img className="img" src='src/Assets/Images/IMG_7877.JPEG' />
+        </div>
+        <div className="img-container">
+        <img className="img" src='src/Assets/Images/IMG_7877.JPEG' />
+        </div>
     </div>
   );
 }
